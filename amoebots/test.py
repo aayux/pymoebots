47e1bf07d07d0c87ddf5a7723df8a490c8e81814
JesseTCS <jt873@scarletmail.rutgers.edu>
2020-02-22 09:26:12 -0500

    bots can be added in mass or individually to grid, visualization for bots and randomized movement with no backend or visual overlap occurring.

--- amoebots/test.py
+++ amoebots/test.py
@@ -1,15 +1,19 @@
 import amoebots.node_construction as nc
 import amoebots.visual_construction as vc
 import amoebots.bot_construction as bc
+import math
 import random as rd
 import time
+import os
+import psutil
 
 
 def create_grid_test():
     t1 = time.process_time()
     n = nc.NodeManager()
-    x = 1000
-    n.create_cluster(x=x)
+    x = 15
+    y = 13
+    n.create_cluster(x=x, y=y)
     v = vc.GridVisualClass()
     v.create_grid(n.plot())
     v.show_fig()
@@ -41,8 +45,77 @@ def scan_for_spaces_test():
     result = bot.scan_for_spaces()
     print(f"{result[0]} is how many agents found, {result[1]} is a list of space end-points")
 
+def create_grid_with_random_bots_and_movement_test():
+    """
+    Notes: remember to add bots data first, then run simulation, and lastly add frames.
+
+    :return:
+    """
+    t1 = time.process_time()
+    n = nc.NodeManager()
+    x = 3
+    y = 3
+    n.create_cluster(x=x, y=y)
+    nodes = n.get_node_array()
+    rd.shuffle(nodes)
+    b = bc.BotManager()
+    number_of_bots = math.ceil(n.get_number_of_nodes() * (1 / 3))
+    b.mass_add_bots(number_of_bots=number_of_bots, nodes=nodes)
+    # for i in range(number_of_bots):
+    #     b.add_bots(node=nodes[i], id=i)
+    # print(f"{b.get_bots()}")
+    positions = b.get_positions()
+    v = vc.GridVisualClass()
+    v.create_bots(positions)
+    b.activate(rounds=10, turns=1)
+    plot = n.plot()
+    # positions = b.get_positions()
+    points = n.get_points()
+    paths = b.get_paths()
+    t2 = time.process_time()
+    this_process = psutil.Process(os.getpid())
+    print(f"This is the time it takes to produce data {t2-t1}, using this much memory {this_process.memory_info().rss} bytes")
+    # v = vc.GridVisualClass()
+    # v.create_bots(positions)
+    v.create_grid(plot)
+    v.create_nodes(points)
+    # b.activate(rounds=20, turns=1)
+
+    v.create_frames(paths)
+    v.create_figure()
+    v.show_fig()
+
+def create_grid_with_random_bots_test():
+    t1 = time.process_time()
+    n = nc.NodeManager()
+    x = 15
+    y = 13
+    n.create_cluster(x=x, y=y)
+    nodes = n.get_node_array()
+    rd.shuffle(nodes)
+    b = bc.BotManager()
+    number_of_bots = math.ceil(n.get_number_of_nodes() * (1 / 3))
+    b.mass_add_bots(number_of_bots=number_of_bots, nodes=nodes)
+    positions = b.get_positions()
+    v = vc.GridVisualClass()
+    v.create_bots(positions)
+    plot = n.plot()
+    points = n.get_points()
+    t2 = time.process_time()
+    this_process = psutil.Process(os.getpid())
+    print(f"This is the time it takes to produce data {t2 - t1}, using this much memory {this_process.memory_info().rss} bytes")
+    v.create_grid(plot)
+    v.create_nodes(points)
+    v.create_figure()
+    v.show_fig()
+
+
+
 
 if __name__ == '__main__':
     # create_grid_test()
-    scan_for_spaces_test()
+    # scan_for_spaces_test()
+    # create_grid_with_random_bots_test()
+    # create_grid_with_random_bots_and_movement_test()
+    pass
 
--- amoebots/visual_construction.py
+++ amoebots/visual_construction.py
@@ -1,13 +1,16 @@
 import plotly.graph_objects as go
 
 
-class GridVisualClass (object):
+class GridVisualClass(object):
     def __init__(self):
         self.__grid_x = None
         self.__grid_y = None
         self.__grid_x_zig = None
         self.__grid_y_zig = None
+        self.__points = None
         self.__data = []
+        self.__frames = []
+        self.__frame_data = []
         self.__fig = None
 
     def create_grid(self, plots):
@@ -22,7 +25,7 @@ class GridVisualClass (object):
             mode="lines",
             name="straight lines",
             line=dict(
-                width=2,
+                width=1,
                 color="blue"
             )
         )
@@ -32,7 +35,7 @@ class GridVisualClass (object):
             mode="lines",
             name="zig-zags",
             line=dict(
-                width=2,
+                width=1,
                 color="blue"
             )
         )
@@ -40,9 +43,89 @@ class GridVisualClass (object):
         self.__data.append(straight_lines)
         self.__data.append(zig_zags)
 
+    def create_nodes(self, points):
+        nodes = go.Scatter(
+            x=[x[0] for x in points],
+            y=[y[1] for y in points],
+            mode="markers",
+            name="Nodes",
+            line=dict(
+                width=2,
+                color="blue"
+            )
+        )
+        self.__data.append(nodes)
+
+    def create_bots(self, bots_list):
+        i=0
+        for bot in bots_list:
+            bots = go.Scatter(
+                x=[bot[0][0], bot[1][0]],
+                y=[bot[0][1], bot[1][1]],
+                name=f"bot {i}",
+                mode="markers",
+                marker=dict(size=10, color="red"),
+                # line=dict(
+                #     width=1,
+                #     color="red",
+                # ),
+                showlegend=False
+            )
+            self.__data.append(bots)
+            i+=1
+
+    def create_frames(self, path):
+        # [[[],[],[],[]],[[],[],[],[]],[[],[],[],[]]]
+        # for bot in range(len(path))
+        # 0 [[],[],[],[]]
+        # x = [path[bot][0][i], path[bot][2][i]]
+        # for i in range(len(path[bot][0]))
+        mode_set = ["markers", "lines+markers"]
+        self.__frames = [go.Frame(
+            data=[go.Scatter(
+                x=[path[i][0][j], path[i][2][j]],
+                y=[path[i][1][j], path[i][3][j]],
+                name=f"bot {i}",
+                mode=mode_set[1],
+                marker=dict(size=10, color="red")) if path[i][0][j] != path[i][2][j] else go.Scatter(
+                x=[path[i][0][j], path[i][2][j]],
+                y=[path[i][1][j], path[i][3][j]],
+                name=f"bot {i}",
+                mode=mode_set[0],
+                marker=dict(size=10, color="red"))
+                for i in range(len(path))])
+            for j in range(len(path[0][0]))]
+        # for j in range(len(path[0][0]))
+        # for i in range(len(path))
+        # for i in range(1)
+        # len(path)
+
+        # self.__frames = [go.Frame(
+        #     data=[go.Scatter(
+        #         x=[path[i][0][0]],
+        #         y=[path[i][0][1]], )
+        #
+        #         for i in range(len(path))])]
+
+    def create_figure(self):
         self.__fig = go.Figure(
             data=self.__data,
+            layout={'title': 'Amoebot Movement Demo',
+                    'updatemenus': [dict(type="buttons",
+                              buttons=[dict(label="Play",
+                                            method="animate",
+                                            args=[None, dict(frame=dict(duration=500, redraw=True),
+                                                             fromcurrent=True,
+                                                             transition=dict(duration=300,
+                                                                             easing='quadratic-in-out'))]),
+                                       dict(label='Pause',
+                                            method='animate',
+                                            args=[[None], dict(frame=dict(duration=0, redraw=False),
+                                                               mode='immediate',
+                                                               transition=dict(duration=0))])])]},
+            frames=self.__frames
         )
 
     def show_fig(self):
         self.__fig.show()
+        # self.__fig.write_html('./Amoebot Movement Demo.html', include_plotlyjs='cdn')
