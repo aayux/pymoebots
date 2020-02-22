a2480a6cd32aa5021fd14772ea6d3704fffda1b1
JesseTCS <jt873@scarletmail.rutgers.edu>
2020-02-22 16:23:50 -0500

    Implemented multi-threading for simultaneous bot activation and fixed some bugs regarding movement.

--- amoebots/test.py
+++ amoebots/test.py
@@ -45,13 +45,13 @@ def scan_for_spaces_test():
     result = bot.scan_for_spaces()
     print(f"{result[0]} is how many agents found, {result[1]} is a list of space end-points")
 
-def create_grid_with_random_bots_and_movement_test():
+def MT_create_grid_with_random_bots_and_movement_test():
     """
     Notes: remember to add bots data first, then run simulation, and lastly add frames.
 
     :return:
     """
-    t1 = time.process_time()
+    t1 = time.perf_counter()
     n = nc.NodeManager()
     x = 3
     y = 3
@@ -67,12 +67,13 @@ def create_grid_with_random_bots_and_movement_test():
     positions = b.get_positions()
     v = vc.GridVisualClass()
     v.create_bots(positions)
-    b.activate(rounds=10, turns=1)
+    # b.activate(rounds=10, turns=1)
+    b.bot_threads(rounds=200, turns=None)
     plot = n.plot()
     # positions = b.get_positions()
     points = n.get_points()
     paths = b.get_paths()
-    t2 = time.process_time()
+    t2 = time.perf_counter()
     this_process = psutil.Process(os.getpid())
     print(f"This is the time it takes to produce data {t2-t1}, using this much memory {this_process.memory_info().rss} bytes")
     # v = vc.GridVisualClass()
@@ -84,6 +85,49 @@ def create_grid_with_random_bots_and_movement_test():
     v.create_frames(paths)
     v.create_figure()
     v.show_fig()
+    print("success")
+
+def create_grid_with_random_bots_and_movement_test():
+    """
+    Notes: remember to add bots data first, then run simulation, and lastly add frames.
+
+    :return:
+    """
+    t1 = time.perf_counter()
+    n = nc.NodeManager()
+    x = 15
+    y = 13
+    n.create_cluster(x=x, y=y)
+    nodes = n.get_node_array()
+    rd.shuffle(nodes)
+    b = bc.BotManager()
+    number_of_bots = math.ceil(n.get_number_of_nodes() * (1 / 3))
+    b.mass_add_bots(number_of_bots=number_of_bots, nodes=nodes)
+    # for i in range(number_of_bots):
+    #     b.add_bots(node=nodes[i], id=i)
+    # print(f"{b.get_bots()}")
+    positions = b.get_positions()
+    v = vc.GridVisualClass()
+    v.create_bots(positions)
+    b.activate(rounds=200, turns=1)
+    # b.bot_threads(rounds=200, turns=None)
+    plot = n.plot()
+    # positions = b.get_positions()
+    points = n.get_points()
+    paths = b.get_paths()
+    t2 = time.perf_counter()
+    this_process = psutil.Process(os.getpid())
+    print(f"This is the time it takes to produce data {t2-t1}, using this much memory {this_process.memory_info().rss} bytes")
+    # v = vc.GridVisualClass()
+    # v.create_bots(positions)
+    v.create_grid(plot)
+    v.create_nodes(points)
+    # b.activate(rounds=20, turns=1)
+
+    v.create_frames(paths)
+    v.create_figure()
+    v.show_fig()
+    print("success")
 
 def create_grid_with_random_bots_test():
     t1 = time.process_time()
@@ -117,5 +161,6 @@ if __name__ == '__main__':
     # scan_for_spaces_test()
     # create_grid_with_random_bots_test()
     # create_grid_with_random_bots_and_movement_test()
+    # MT_create_grid_with_random_bots_and_movement_test()
     pass
 
