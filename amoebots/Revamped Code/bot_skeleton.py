13473d1cd4b3d250f4edceef4b6ffab883c979cc
Joshua Barnett <jbarnetj@gmail.com>
2020-03-07 11:08:04 -0500

    Added comments

--- amoebots/Revamped Code/bot_skeleton.py
+++ amoebots/Revamped Code/bot_skeleton.py
@@ -7,6 +7,7 @@ import concurrent.futures as cf
 
 @dataclass
 class Bot:
+    """ This class is responsible for a bot's general attributes and functions"""
     # general information
     bot_id: int = field(default=None)
     port_structure: np.ndarray = field(default=None)
@@ -111,6 +112,7 @@ class Bot:
         return np.uint8(1)
 
     def publish(self, agent_id=None, item=None):
+        """This method writes information (ex tokens)to the selected agent"""
         self.publishing[agent_id] = item
 
     def scan_for_spaces(self):
