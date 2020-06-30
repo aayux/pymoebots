d990b56361e9b7ee65b504bfe8edaad2313c4311
Aayush Yadav <aayushyadav96@gmail.com>
2020-06-29 21:29:48 -0400

    Multiprocessing fix (#9)

---
+++ amoebot/elements/bot/agent.py
@@ -0,0 +1,88 @@
+import numpy as np
+from numpy import uint8, array
+
+from .core import Amoebot
+
+# from ..functional.nwobjects import MessagePipe, Packet, Token
+# from ..functional.mesghandler import MessageHandler
+
+class Agent(Amoebot):
+    r""" all amoebot algorithms are written as member functions of class `Agent`
+    """
+
+    def __init__(self, __bot_id:int, head:object):
+        super().__init__(__bot_id, head)
+
+    def execute(self) -> tuple:
+        r"""
+        Worker function for each amoebot. Intended to manage parallelisation 
+        between agents and attach algorithmic modules to the amoebot.
+
+        returns: np.uint8: execution status
+        """
+
+        # if the agent is active
+        if self.active:
+
+            # run one step of an elementary algorithm
+            self.move_agent()
+
+            # reset the poisson clock
+            self.active = self._reset_clock(refresh=True)
+
+            return (self, uint8(1))
+
+        else:
+            # decrement the clock and get active status
+            self.active = self._reset_clock(refresh=False)
+
+            return (self, uint8(0))
+    
+    def compress_agent(self):
+        r"""
+        """
+        raise NotImplementedError
+    
+    def move_agent(self, direction:str=None):
+        r"""
+        Simple (random) movement algorithm for the amoebot model.
+
+        direction : direction of movement (one of port labeling); if no 
+                    value provided, movement is performed randomly.
+
+        returns: None
+        """
+
+        # contract an expanded particle
+        if  self.head is not self.tail:
+            pull = self.tail
+            self.tail = self.head
+            pull.departure()
+        
+        # expand a contracted particle
+        else:
+            # get all available ports in scan order
+            open_ports = self.get_open_ports
+
+            # select a random direction when none provided
+            if direction is None and open_ports: 
+                direction = np.random.choice(open_ports)
+            
+            # move to indicated direction if available
+            if direction in open_ports:
+                push = self.head.get_neighbor(direction)
+                self.head = push
+                self.head.arrival(bot=self)
+
+    @property
+    def get_open_ports(self) -> list:
+        r""" a list of open ports for bot to move to
+        """
+        open_port_list = list()
+        for port in self.head.get_ports:
+            
+            node = self.head.get_neighbor(port)
+            if (node is not None) and (not node.get_occupied):
+                open_port_list.append(port)
+
+        return open_port_list
--- amoebot/elements/bot/manager.py
+++ amoebot/elements/bot/manager.py
@@ -1,102 +1,179 @@
-import sys
+import logging
 import numpy as np
+import multiprocessing
 
-from concurrent import futures
-from numpy import ndarray, uint8
+from numpy import array, ndarray, uint8
+from multiprocessing import managers
 
-from .core import Amoebot
-from ...extras.structures import AnonList
-from ..node.core import Node
-from ...extras.exceptions import InitializationError
+from .agent import Agent
 from ..manager import Manager
+from ..tracker import StateTracker
+from ...extras.structures import AnonList
 
 class AmoebotManager(Manager):
-    r"""
-    manages thread assignment to agents and allocates functionals to bots
+    r""" 
+    Manages sequential and/or asynchronous task assignment to agents of 
+    individual amoebots.
     """
 
-    def __init__(self):
+    def __init__(self, config_num:str):
         # holds all `Amoebot` objects created anonymously
-        self.amoebots:AnonList = AnonList()
+        self.amoebots: object = AnonList()
 
-        # truth values signalling execution status of each bot, 1 when complete
-        self.status: dict = {0: list(), 1: list()}
+        # object of class `StateTracker`
+        self.tracker: object = StateTracker(config_num)
 
-    def _add_bot(self, node:Node):
+    def exec_async(self, n_cores:int, max_iter:int, buffer_len:int=10):
+        # create a process safe queue
+        mp_manager = multiprocessing.Manager()
+        ip_buffr = mp_manager.Queue()
+        op_buffr = mp_manager.Queue()
+
+        # start the process that manages the shared memory blocks
+        shared = mp_manager.Namespace()
+
+        # create a lock for the shared namespace
+        lock = mp_manager.Lock()
+
+        # create a shared list of amoebots
+        shared.amoebots = self.amoebots
+
+        # uncomment for stdout logging
+        # mp_logger = multiprocessing.log_to_stderr()
+        # mp_logger.setLevel(multiprocessing.SUBDEBUG)
+
+        _exec_async(self.tracker, ip_buffr, op_buffr, shared, 
+                    lock, n_cores, max_iter, buffer_len)
+
+    def exec_sequential(self, max_iter:int) -> dict:
         r"""
-        adds individual amoebots to the `AnonList`
         """
+        for _ in range(max_iter):
+            for __id, _ in enumerate(self.amoebots):
+                self.amoebots[__id] = _exec_step(amoebots=self.amoebots[__id])
 
-        bot = Amoebot(head=node)
-        self.amoebots.insert(bot)
-
-    # TODO: move placement to separate class
-    def random_placement(self, n_bots:int, node_list:list):
-        r"""
-        places bots on an instance of `elements.node.core.Node` of the 
-        triangular graph randomly
+    def _add_bot(self, __id:int, node:object):
+        r""" adds individual particles to the (partially anonymous) `AnonList`
         """
+        agent_of_amoebot = Agent(__id, head=node)
+        self.amoebots.insert(agent_of_amoebot, __id)
 
-        node_list = np.asarray(node_list)
-        np.random.shuffle(node_list)
 
-        # add bot to the list at random node position
-        for ix in range(n_bots): self._add_bot(node_list[ix])
+def _exec_async(
+                tracker:object, 
+                ip_buffr:object, 
+                op_buffr:object, 
+                shared:object, 
+                lock:object, 
+                n_cores:int, 
+                max_iter:int, 
+                buffer_len:int
+            ):
+    r"""
+    Set up asynchronous calls for bot execution and create a queueing 
+    manager for writing the state configuration file.
 
-    def _activate(self, amoebot:Amoebot):
-        r"""
-        execute one timestep of `elements.bots.core.Amoebot.execute()`
-        """
-        # TODO: required? copy variables and methods to avoid contention
-        status = self.status
+    returns (dict) : execution status
+    """
 
-        try:
-            # execute the amoebot algorithms and update execution status
-            status[amoebot.execute()] = amoebot
-        except IndexError:
-            raise InitializationError(
-                                f"Amoebots have not been initialized ",
-                                f"correctly, check file and retry. Exiting!"
-            )
-            sys.exit(0)
+    with multiprocessing.Pool(processes=n_cores) as pool:
+        # create a listener process that handles queued writes
+        _ = pool.apply_async(_queueing_manager, (op_buffr, shared, tracker, ))
 
-        self.status = status
+        # generate an initial list of jobs to be completed
+        jobs_init = [[__id] for __id, _ in enumerate(shared.amoebots)]
+        for job in jobs_init: ip_buffr.put(job)
 
-    def m_activate(self) -> uint8:
-        r"""
-        sets up threaded calls to activation methods for all bots
+        results = list()
 
-        returns: np.uint8:   execution status
-        """
-        # TODO: required? copy variables and methods to avoid contention
-        activate = self._activate
+        # execute jobs on agents for fixed number of activations
+        while not ip_buffr.empty():
+            results.append(pool.apply_async(_exec_step, (
+                                                    ip_buffr, op_buffr, 
+                                                    shared, lock
+                                                )))
 
-        with futures.ThreadPoolExecutor() as executor:
+            # break if maximum iterations have been reached
+            if len(results) >= max_iter: break
 
-            # maps range to active method
-            executor.map(activate, self.amoebots)
+            # flush the queue if buffer_len reached
+            # elif not (len(results) % buffer_len):
 
-        status = self.status
+            #     # collect results from the pool
+            #     for result in results: result.get()
 
-        # return 1 if everything ran successfully
-        if len(status[0]) == 0: return uint8(1)
+            #     # clear the queue using the listener process
+            #     op_buffr.put(['ps-reset', 0])
 
-        return uint8(0)
+            #     # empty the results list and decrement max_iter
+            #     results = list()
+            #     max_iter -= buffer_len
 
-    def s_activate(self) -> uint8:
-        r"""
-        Sequential bot activations; activations are arbitrary as long as each 
-        amoebot gets a chance to run during a cycle.
+        # collect remaining results from the pool
+        for result in results: result.get()
 
-        returns: np.uint8:   execution status
-        """
+        # exit using the listener process
+        op_buffr.put(['ps-kill', 0])
 
-        # one cycle of activation per amoebot
-        for amoebot in self.amoebots: self._activate(amoebot)
 
-        status = self.status
+def _exec_step(
+        ip_buffr:multiprocessing.managers.AutoProxy=None, 
+        op_buffr:multiprocessing.managers.AutoProxy=None, 
+        shared:multiprocessing.managers.NamespaceProxy=None, 
+        lock:multiprocessing.managers.AcquirerProxy=None, 
+        amoebot:Agent=None
+    ):
+    r""" execute one step of `elements.bots.core.Amoebot.execute()`
+    """
 
-        # return 1 if everything ran successfully
-        if len(status[0]) == 0: return uint8(1)
+    # asynchronous execution mode
+    if shared is not None:
+        __id = ip_buffr.get()[0]
+        
+        # copy the correct reference to current amoebot object
+        amoebot = shared.amoebots[__id]
 
-        return uint8(0)
+        # execute the amoebot algorithm(s) and update activation status
+        amoebot, actv_status = amoebot.execute()
+
+        # protect the shared memory from accidental overwrites
+        lock.acquire(blocking=True)
+
+        # write the returned reference back to shared memory
+        shared_amoebots = shared.amoebots
+        shared_amoebots[__id] = amoebot
+        shared.amoebots = shared_amoebots
+
+        lock.release()
+
+        # push the status information to the queues
+        ip_buffr.put([__id])
+        op_buffr.put([__id, actv_status])
+
+    # sequential execution mode
+    else:
+        # execute the amoebot algorithm(s) and update activation status
+        amoebot, _ = amoebot.execute()
+        return amoebot
+
+def _queueing_manager(op_buffr:object, shared:object, tracker:object):
+    while True:
+        # pop the leading entry in the queue
+        response = op_buffr.get()
+        
+        # reset or exit the listener process
+        if response[0] == 'ps-kill': break
+        # elif response[0] == 'ps-reset': return
+
+        __id, actv_status = response
+
+        # copy the reference to current amoebot object
+        amoebot = shared.amoebots[__id]
+
+        # load state information
+        state = (amoebot.head, amoebot.tail, amoebot.clock)
+
+        # if there was an activation for this amoebot
+        if actv_status:
+            # call `StateTracker.update` to update the configuration
+            tracker.update(__id, state)
